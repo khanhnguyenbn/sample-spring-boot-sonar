5bda522b3368b4087448e8cc0e2424a96b9dfb1b
Khanh Nguyen <khanh.nguyenba.97bn@gmail.com>
2025-03-08 15:59:53 +0700

    test ignore duplicate code for test folder

--- src/test/java/com/example/sample_spring_boot_sonar/SampleSpringBootSonarApplicationTests.java
+++ src/test/java/com/example/sample_spring_boot_sonar/SampleSpringBootSonarApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.sample_spring_boot_sonar;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class SampleSpringBootSonarApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("test duplicated code");
+		log.info("test duplicated code");
+		log.info("test duplicated code");
+		log.info("test duplicated code");
+		log.info("test duplicated code");
 	}
 
 }
