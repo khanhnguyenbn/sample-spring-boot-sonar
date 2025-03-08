d9cdc7a33ed3bb0aa7238c8610fbb8284d367cd8
Khanh Nguyen <khanh.nguyenba.97bn@gmail.com>
2025-03-08 10:35:18 +0700

    first commit

---
+++ src/main/java/com/example/sample_spring_boot_sonar/SampleSpringBootSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample_spring_boot_sonar/SampleSpringBootSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
